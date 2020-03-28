51010b68bac5d4ae09deed9529b7081d2efbfce9
anuj <anujshah584@gmail.com>
2020-03-28 18:29:39 +0530

    fix: mobile responsiveness

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -13,41 +13,36 @@ class NavBar extends Component {
   render() {
     const { user } = this.props.auth;
     return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper">
-            <Link to="/" className="brand-logo">
-              <i className="material-icons">code</i>Tomato
-            </Link>
-            {Object.keys(user || {}).length !== 0 ? (
-              <ul
-                id="nav-mobile"
-                className="right hide-on-med-and-down list-margin"
-              >
-                <li>
-                  <Link to="/restaurants/create">
-                    Create Restaurants And Meals
-                  </Link>
-                </li>
+      <nav className="z-depth-0">
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo hide-on-med-and-down">
+            <i className="material-icons">code</i>Tomato
+          </Link>
+          {Object.keys(user || {}).length !== 0 ? (
+            <ul id="nav-mobile" className="right list-margin">
+              <li>
+                <Link to="/restaurants/create">
+                  Create Restaurants And Meals
+                </Link>
+              </li>
 
-                <li>
-                  <Link to="/restaurants">All Restaurants</Link>
-                </li>
-                <li>
-                  <Link to="/orders">Your Orders</Link>
-                </li>
-                <li>
-                  <b>Hey there,</b> {(user.name || "").split(" ")[0]}
-                </li>
-                <li>
-                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                  <a onClick={this.onLogoutClick}>Logout</a>
-                </li>
-              </ul>
-            ) : null}
-          </div>
-        </nav>
-      </div>
+              <li>
+                <Link to="/restaurants">All Restaurants</Link>
+              </li>
+              <li>
+                <Link to="/orders">Your Orders</Link>
+              </li>
+              <li>
+                <b>Hey there,</b> {(user.name || "").split(" ")[0]}
+              </li>
+              <li>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a onClick={this.onLogoutClick}>Logout</a>
+              </li>
+            </ul>
+          ) : null}
+        </div>
+      </nav>
     );
   }
 }
