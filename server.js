1a91b34b139e50a43ced71fd195d3322bfef147f
anuj <anujshah584@gmail.com>
2020-03-28 16:09:55 +0530

    feat: add heroku build script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/user");
 const restaurants = require("./routes/api/restaurant");
@@ -38,6 +39,16 @@ app.use("/api/users", users);
 app.use("/api/restaurants", restaurants);
 app.use("/api/meals", meals);
 app.use("/api/orders", orders);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
